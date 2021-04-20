ac0685c4aba31ac55ac72221a1b2b00bf6a10997
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-04-19 23:26:28 -0300

    Arquivos iniciais

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Button title="Test1"/>
+    <Button title="Test2"/>
+    <Button title="Test3"/>
+    <Button title="Test4"/>
+    </>
   );
 }
 
